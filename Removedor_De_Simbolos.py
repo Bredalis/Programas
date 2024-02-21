3f11544c9b7b580bc5e139a19cb073d5c3fe0241
PRO X <bredalisgautreaux@gmail>
2024-02-21 08:17:42 +0100

    Se modifico un programa

--- Removedor_De_Simbolos.py
+++ Removedor_De_Simbolos.py
@@ -24,8 +24,8 @@ class RemplazarSimbolos:
 
 # Modifica la url y los simbolos
 
-# archivo = RemplazarSimbolos('C:/Users/Bradalis/Downloads/Archivo.txt')
+# archivo = RemplazarSimbolos('C:/Users/Bradalis/Downloads/Borrar.txt')
 # archivo.abrir_archivo()
-# archivo.remplazar_simbolos('#', ' ')
+# archivo.remplazar_simbolos('*', ' ')
 # archivo.guardar_archivo()
 # archivo.mostrar_archivo()
